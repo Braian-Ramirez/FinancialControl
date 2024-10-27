3306b43251a05d71f6b50a77a047b9b189962553
Vivi271 <142178270+Vivi271@users.noreply.github.com>
2024-10-27 13:38:27 -0500

    Creación de la carpeta src, se agregó dentro de ella las carpetas models y routes; dentro de models se agregó el archivo usuario.js con su respectivo código.

---
+++ src/models/usuarios.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose"); // importando el componente mongoose
+const bcrypt = require("bcrypt"); // importando el componente bcrypt
+
+const userSchema = mongoose.Schema({
+  nombre: {
+    type: String,
+    required: true,
+  },
+  correo: {
+    type: String,
+    required: true,
+  },
+  contraseña: {
+    type: String,
+    required: true,
+  },
+  contraseñaConfirmacion: {
+    type: String,
+    required: true,
+  },
+  telefono: {
+    type: Number,
+    required: true,
+  },
+  fechaNacimiento: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Método para encriptar la contraseña
+userSchema.methods.encryptClave = async (clave) => {
+  const salt = await bcrypt.genSalt(10);
+  return bcrypt.hash(clave, salt);
+};
+
+module.exports = mongoose.model("Usuario", userSchema);
