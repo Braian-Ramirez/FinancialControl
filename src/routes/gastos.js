eca9e26cd4f7e13a09ab16286d48bab65ef75fc8
Vivi271 <142178270+Vivi271@users.noreply.github.com>
2024-10-27 14:53:33 -0500

    Crear el get dentro de routes/gastos.

--- src/routes/gastos.js
+++ src/routes/gastos.js
@@ -11,3 +11,18 @@ router.post("/gastos", (req, res) => {
         .then((data) => res.json(data))
         .catch((error) => res.status(400).json({ message: error.message }));
 });
+
+ //Actualizar datos de gasto
+router.put("/gastos/:id", (req, res) => {
+    const { id } = req.params;
+    const { nombre, descripcion, monto, fechaPago } = req.body;
+    gastoSchema
+        .updateOne({ _id: id }, { $set: { nombre, descripcion, monto, fechaPago } })
+        .then((data) => {
+            if (data.modifiedCount === 0) {
+                return res.status(404).json({ message: "Gasto no encontrado o no actualizado" });
+            }
+            res.json(data);
+        })
+        .catch((error) => res.status(400).json({ message: error.message }));
+});
