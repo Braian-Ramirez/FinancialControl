01509594bb667883248dfceda32661957ff21fbd
Braian F. Ramírez <bfrosm@hotmail.com>
2024-10-27 13:40:11 -0500

    Se realizo la conexión con mongodb

---
+++ index.js
@@ -0,0 +1,31 @@
+const parser = require("body-parser");
+const express = require('express');
+const app = express();
+const port = 4000;
+const ingresosRoutes = require("./routes/ingresos");
+const gastosRoutes = require("./routes/gastos");
+const costosRoutes = require("./routes/costos");
+const usuariosRoutes = require("./routes/usuarios");
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+app.use(parser.urlencoded({ extended: false }));
+app.use(parser.json());
+app.use(express.json());
+
+// Gestión de rutas usando middleware
+app.use("/api/ingresos", ingresosRoutes); // rutas para ingresos
+app.use("/api/gastos", gastosRoutes); // rutas para gastos
+app.use("/api/costos", costosRoutes); // rutas para costos
+app.use("/api/usuarios", usuariosRoutes); // rutas para registro de usuario
+
+// Conexión a la base de datos
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => console.log("Conexión exitosa"))
+    .catch((error) => console.log(error));
+
+// Conexión al puerto
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+});
