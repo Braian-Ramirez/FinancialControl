0addf74520817d99d042e94b2f01b217b5bb8c03
Angel <angels.pinzons@konradlorenz.edu.co>
2024-10-28 00:31:24 -0500

    Se culmina el CRUD de costos route
    Agregando el put y el delete respectivamente

--- src/routes/costos.js
+++ src/routes/costos.js
@@ -31,4 +31,29 @@ router.get("/costos", (req, res) => {
       .catch((error) => res.status(500).json({ message: error.message }));
   });
 
+  // Actualizar un costo por ID (Update)
+router.put("/costos/:id", (req, res) => {
+    const { id } = req.params;
+    const { nombre, descripcion, monto, fechaPago } = req.body;
+    costoSchema
+      .updateOne({ _id: id }, { $set: { nombre, descripcion, monto, fechaPago } })
+      .then((data) => {
+        if (data.modifiedCount === 0) return res.status(404).json({ message: "Costo no actualizado o no encontrado" });
+        res.json(data);
+      })
+      .catch((error) => res.status(400).json({ message: error.message }));
+  });
+  
+  // Eliminar un costo por ID (Delete)
+  router.delete("/costos/:id", (req, res) => {
+    const { id } = req.params;
+    costoSchema
+      .deleteOne({ _id: id })
+      .then((data) => {
+        if (data.deletedCount === 0) return res.status(404).json({ message: "Costo no encontrado" });
+        res.json({ message: "Costo eliminado" });
+      })
+      .catch((error) => res.status(500).json({ message: error.message }));
+  });
+
 module.exports = router;
